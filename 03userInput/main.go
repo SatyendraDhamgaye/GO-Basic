0f9ea6a628ce7e75236f2a5ada057b0b47712afd
SatyendraDhamgaye <satyendradhamgaye2@gmail.com>
2023-12-21 16:35:43 +0530

    input

--- 03userInput/main.go
+++ 03userInput/main.go
@@ -10,6 +10,10 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Enter a value")
+
+	// comma ok // comma err
+	// Syntax: variable, error := reader.Read('\n') where variable is the value and error if someting goes wrong
+
 	value, _ := reader.ReadString('\n')
 	fmt.Print("Entered value is ", value)
 	fmt.Printf("The type of the enterd value is %T", value)
