b730c2584a00983aac0782207f63e811b39fc37d
SatyendraDhamgaye <satyendradhamgaye2@gmail.com>
2023-12-21 16:20:51 +0530

    variablesFinal

--- 02Variables/main.go
+++ 02Variables/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+//global variables
+var globalvar uint = 10
+
+//constant variavle
+const constantvar float64 = 538.452
+
 func main() {
+
+	//default variables
+
 	var name string = "David"
 	fmt.Println(name)
 	fmt.Printf("The above value is of %T type \n", name)
@@ -22,4 +31,28 @@ func main() {
 	var validation bool = false
 	fmt.Println(validation)
 	fmt.Printf("The above value is of %T type \n", validation)
+
+	//default variables with no values
+
+	var nothing string
+	fmt.Println(nothing)
+	fmt.Printf("The above value is of %T type \n", nothing)
+
+	//with no datatype
+
+	var notype = "abc"
+	fmt.Println(notype)
+	fmt.Printf("The above value is of %T type \n", notype)
+
+	//no var type direct approch
+
+	direct := 10.5
+	fmt.Println(direct)
+	fmt.Printf("The above value is of %T type \n", direct)
+
+	fmt.Println(globalvar)
+	fmt.Printf("The above value is of %T type \n", globalvar)
+
+	fmt.Println(constantvar)
+	fmt.Printf("The above value is of %T type \n", constantvar)
 }
