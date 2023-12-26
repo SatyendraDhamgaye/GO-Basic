9cabfb24aacf3fbd406ac2f39f07af1304037007
SatyendraDhamgaye <satyendradhamgaye2@gmail.com>
2023-12-26 18:04:20 +0530

    Module!!!!!!

---
+++ 23modules/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	greater()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serverBase)
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
+
+func greater() {
+	fmt.Println("Using Gorilla mod")
+}
+
+func serverBase(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>USing Gorrilaa mod </h1>"))
+
+}
