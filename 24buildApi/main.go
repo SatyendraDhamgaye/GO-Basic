b53d5138cd03357a6bf7238d86b53a1bd50b0cc9
SatyendraDhamgaye <satyendradhamgaye2@gmail.com>
2023-12-30 13:01:37 +0530

    api2

--- 24buildApi/main.go
+++ 24buildApi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Shop struct {
@@ -28,6 +30,7 @@ func main() {
 
 }
 
+// controller starts
 func serverHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hi this is API Building first page with nothing after '/' :)</h1>"))
 }
@@ -37,3 +40,20 @@ func getShopDetails(w http.ResponseWriter, r http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(shop)
 }
+
+func getOneShopData(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Getting values from shop")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, course := range shop {
+		if course.Item_Name == params["name"] {
+			json.NewEncoder(w).Encode(shop)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No data found, please enter correct data")
+	return
+}
+
+//controller ends
