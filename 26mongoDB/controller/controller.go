b762a610579e14103eca1ef799839c339b9fc97f
SatyendraDhamgaye <satyendradhamgaye2@gmail.com>
2024-01-03 11:51:09 +0530

    operatins01

--- 26mongoDB/controller/controller.go
+++ 26mongoDB/controller/controller.go
@@ -3,8 +3,11 @@ package controller
 import (
 	"context"
 	"fmt"
-	"log"
 
+	"github.com/SatyendraDhamgaye/mongoDbApi/helpers"
+	"github.com/SatyendraDhamgaye/mongoDbApi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
@@ -30,9 +33,7 @@ func init() {
 	//TODO works in foreground
 	client, err := mongo.Connect(context.TODO(), clientOption)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	helpers.ErrorCatcher(err)
 
 	fmt.Println("MongoDB connection success")
 
@@ -40,4 +41,41 @@ func init() {
 
 	//collection instance
 	fmt.Println("Collection instance is ready")
+}
+
+//mongodb helper methods should go in seperate file
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	helpers.ErrorCatcher(err)
+	fmt.Println("Inserted 1 movie in the database with Id: ", inserted.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	helpers.ErrorCatcher(err)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watcher": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	helpers.ErrorCatcher(err)
+
+	fmt.Println("Modified count: ", result.ModifiedCount)
+}
+
+func deleteOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	helpers.ErrorCatcher(err)
+	filter := bson.M{"_id": id}
+	deletecount, err := collection.DeleteOne(context.Background(), filter)
+	helpers.ErrorCatcher(err)
+	fmt.Println("Movie got deleted with delete count: ", deletecount)
+}
+
+func deleteAllMovie(movieId string) {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}})
+	helpers.ErrorCatcher(err)
+	fmt.Println("Number of movies deleted: ", deleteResult.DeletedCount)
+
 }
